c1b5ba096494a564d1040c70f95734f79988d75a
Thomas Bailey <t1bailey@ucsd.edu>
2025-02-25 11:41:22 -0800

    Uploading files

---
+++ class4/class0.4.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "BIMM 143 class 4: Intro to R"
+#' author: "Thomas Bailey"
+#' date: "January 16, 2025"
+#' ---
+
+
+x<- 1:50
+plot(x, sin(x))
+
+#make this nicer
+plot(x, sin(x), col="blue", typ="l", lwd=3 )
+
